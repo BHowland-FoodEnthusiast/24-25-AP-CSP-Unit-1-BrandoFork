b16807ab61aef58521b911b98b090d9d83575c16
BHowland-FoodEnthusiast <brandon.howland@vtsd.com>
2024-09-19 11:46:07 -0400

    simple first edit, makes triangle bigger.

---
+++ 1-1-4/Example.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+
+if num1 > num2:
+    print("Number 1 is bigger")
+
+elif num2 > num1:
+    print("Number 2 is bigger")
+elif num2 == num1:
+    print("Numbers are equal")
+
---
+++ 1-1-4/grades.py
@@ -0,0 +1,16 @@
+grade=int(input("Enter a grade you got: "))
+
+if grade >= 90:
+    print("You got an A")
+
+elif grade >= 80:
+    print("You got a B")
+
+elif grade >= 70:
+    print("You got a C")
+
+elif grade >= 65:
+    print("You got a D")
+
+else:
+    print("You got an F")
