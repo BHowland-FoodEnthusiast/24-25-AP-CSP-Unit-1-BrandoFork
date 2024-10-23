73b9be6bd8d0622723efcb4408917bed9b26e551
BHowland-FoodEnthusiast <brandon.howland@vtsd.com>
2024-10-23 12:35:59 -0400

    Music Algorithm better odds, should be optimally fair.

--- 1-1-9/project.py
+++ 1-1-9/project.py
@@ -7,16 +7,6 @@ def init():
     xpos = 480
     ypos = 300
     clouds.speed(0)
-
-
-clouds = trtl.Turtle()
-clouds.penup()
-
-
-
-init()
-clouds.circle(360, 360, 360)
-# making and coloring clouds
 def cloud_draw():
     global xpos, ypos
     for cloud in range(3):
@@ -37,32 +27,45 @@ def cloud_draw():
 
         clouds.penup()
         clouds.pendown()
+
+
+clouds = trtl.Turtle()
+clouds.penup()
+init()
+
+
+# making and coloring clouds
+
 #making three clouds
 for cloud in range(3):
     xpos -= 150
     ypos = 300
     cloud_draw()
-#painting the ground
+
+#method for painting the grass
 def grass_draw():
     global xpos, ypos
-    xpos = -470
+    xpos = -500
     ypos = -400
     clouds.penup()
     clouds.goto(xpos, ypos)
     clouds.pendown()
     clouds.left(90)
+
+#paints the grass
 grass_draw()
 clouds.color('#038c25')
-for range in range(700):
+for range in range(1000):
     clouds.forward(rd.randint(50, 100))
-    xpos += 2
+    xpos += 1
     clouds.pendown()
     clouds.goto(xpos, ypos)
     clouds.penup()
 
 
 
-wn = trtl.Screen()
+
+
 wn = trtl.Screen()
 #painting the sky
 wn.bgcolor('#B2BEB5')
